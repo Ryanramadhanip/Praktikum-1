7188719feb365e09a6a52811e8fb83194ba395b1
Ryanramadhanip <42019683+Ryanramadhanip@users.noreply.github.com>
2020-11-26 13:02:09 +0700

    Add files via upload

---
+++ VariabelKonstanta.js
@@ -0,0 +1,27 @@
+let nama = "Ryan" //menyimpan data string
+let umur = 18 //menyimpan data numerik
+let status_menikah = false //menyimpan data boolean
+
+const url = "http://smktelkom-mlg.sch.id"
+const port = 8080
+
+let siswa1 = {
+    nis: "101",
+    nama: "Suneo",
+    jurusan: "RPL"
+}
+
+let siswa2 = {
+    nis: "102",
+    nama: "Jeko",
+    jurusan: "TKJ"
+}
+
+//menampilkan nama siswa1
+console.log("Nama Siswa 1 = " + siswa1.nama);
+
+//mengubah nama siswa 1 menjadi Ijat
+siswa1.nama = "Nobita"
+
+//menampilkan nama setelah diubah
+console.log("Nama Siswa 1 = " + siswa1.nama);
